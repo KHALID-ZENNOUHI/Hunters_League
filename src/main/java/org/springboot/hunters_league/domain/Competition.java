7aec80fe7cfb1c0909e0a7940fd67660ca094aa1
khalid.zennouhi <144844220+KHALID-ZENNOUHI@users.noreply.github.com>
2024-11-07 19:10:47 +0100

    add the fetch type lazy to the list of participations

--- src/main/java/org/springboot/hunters_league/domain/Competition.java
+++ src/main/java/org/springboot/hunters_league/domain/Competition.java
@@ -6,8 +6,8 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+import org.springboot.hunters_league.domain.Enum.SpeciesType;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -47,7 +47,7 @@ public class Competition {
     @NotNull(message = "Open registration status cannot be null.")
     private Boolean openRegistration;
 
-    @OneToMany(mappedBy = "competition")
+    @OneToMany(mappedBy = "competition", fetch = FetchType.LAZY)
     private List<Participation> participations;
 
 }
