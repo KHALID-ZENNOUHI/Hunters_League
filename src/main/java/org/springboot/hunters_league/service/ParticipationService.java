e0d06eb6122faf4dd25d753c36ecdd4a82ae78fc
khalid.zennouhi <144844220+KHALID-ZENNOUHI@users.noreply.github.com>
2024-11-11 09:45:24 +0100

    feat(CompetitionService): add methods for participation registration, score calculation, and competition results
    - Implemented `registerParticipant` method in `CompetitionService`:
      - Validates license expiration date before registration.
      - Checks if the competition registration is currently open.
    
    - Added `calculateScore` method:
      - Computes scores based on competition-specific criteria.
    
    - Developed methods to retrieve competition results:
      - `getCompetitionResultByCompetitionId` - Fetches results for a specific competition by its ID.
      - `getCompetitionResultByUserId` - Retrieves a user's competition results based on their ID.
      - `getPodium` - Provides the top 3 participants in a competition.
      - `getCompetitionHistory` - Returns a history of results for all competitions.
    
    This update enhances competition participation workflows by introducing validation checks, score calculation, and diverse result retrieval options.

--- src/main/java/org/springboot/hunters_league/service/ParticipationService.java
+++ src/main/java/org/springboot/hunters_league/service/ParticipationService.java
@@ -1,15 +1,27 @@
 package org.springboot.hunters_league.service;
 
+import jakarta.persistence.criteria.Predicate;
 import org.springboot.hunters_league.domain.Competition;
 import org.springboot.hunters_league.domain.Participation;
 import org.springboot.hunters_league.domain.User;
 import org.springboot.hunters_league.repository.ParticipationRepository;
 import org.springboot.hunters_league.service.dto.ParticipationDTO;
 import org.springboot.hunters_league.web.error.CompetitionRegistrationClosedException;
+import org.springboot.hunters_league.web.error.NoSearchCriteriaException;
+import org.springboot.hunters_league.web.error.ParticipationNotFoundException;
 import org.springboot.hunters_league.web.error.UserLicenseExpiredException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Service
@@ -44,5 +56,59 @@ public class ParticipationService {
             if (!competition.getOpenRegistration()) {
                 throw new CompetitionRegistrationClosedException();
             }
+    }
+
+    public Page<Participation> findAll(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("Competition.date").descending());
+        return participationRepository.findAll(pageable);
+
+    }
+
+    public Participation findById(UUID id) {
+        return participationRepository.findById(id).orElseThrow(ParticipationNotFoundException::new);
+    }
+
+    public Double calculateScore(UUID id) {
+        Participation participation = findById(id);
+        Double score = participation.getHunts().stream().mapToDouble(hunt -> hunt.getSpecies().getPoints() + hunt.getWeight() * hunt.getSpecies().getCategory().getValue() * hunt.getSpecies().getDifficulty().getValue()).sum();
+        participation.setScore(score);
+        participationRepository.save(participation);
+        return score;
+    }
+
+    public List<Participation> findByCriteria(UUID userId, UUID competitionId) {
+
+        Specification<Participation> spec = (root, query, cb) -> {
+
+            List<Predicate> predicates = new ArrayList<>();
+
+
+            if (userId != null) {
+                predicates.add(cb.equal(root.get("user").get("id"), userId));
+            }
+
+
+            if (competitionId != null) {
+                predicates.add(cb.equal(root.get("competition").get("id"), competitionId));
+            }
+
+            if (predicates.isEmpty()) {
+                throw new NoSearchCriteriaException();
+            }
+
+            return cb.and(predicates.toArray(new Predicate[predicates.size()]));
+        };
+
+        return participationRepository.findAll(spec);
+    }
+
+    public List<Participation> competitionPodium(UUID competitionId) {
+        competitionService.findById(competitionId);
+        return participationRepository.findTop3ByCompetition(competitionId);
+    }
+
+    public List<Participation> findParticipationHistory(UUID userId) {
+        userService.findById(userId);
+        return participationRepository.findParticipationHistory(userId);
     }
 }
--- src/main/java/org/springboot/hunters_league/web/api/ParticipationController.java
+++ src/main/java/org/springboot/hunters_league/web/api/ParticipationController.java
@@ -4,22 +4,19 @@ import jakarta.validation.Valid;
 import org.springboot.hunters_league.domain.Participation;
 import org.springboot.hunters_league.service.ParticipationService;
 import org.springboot.hunters_league.service.dto.ParticipationDTO;
-import org.springboot.hunters_league.service.dto.mapper.ParticipationMapper;
 import org.springboot.hunters_league.web.vm.mapper.CompetitionMapper;
+import org.springboot.hunters_league.web.vm.mapper.ParticipationMapper;
 import org.springboot.hunters_league.web.vm.mapper.UserMapper;
-import org.springboot.hunters_league.web.vm.responseVM.CompetitionVM;
 import org.springboot.hunters_league.web.vm.responseVM.ParticipationVM;
-import org.springboot.hunters_league.web.vm.responseVM.ProfileVM;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Locale;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/participations")
+@RequestMapping("/api/v1/participation")
 public class ParticipationController {
     ParticipationService participationService;
     ParticipationMapper participationMapper;
@@ -36,12 +33,38 @@ public class ParticipationController {
     @PostMapping
     public ResponseEntity<ParticipationVM> inscription(@Valid @RequestBody ParticipationDTO participationDTO) {
         Participation savedParticipation = participationService.inscription(participationDTO);
-        ProfileVM user = userMapper.userToProfileVM(savedParticipation.getUser());
-        CompetitionVM competition = competitionMapper.competitionToCompetitionVM(savedParticipation.getCompetition());
-        ParticipationVM participationVM = new ParticipationVM();
-        participationVM.setId(savedParticipation.getId());
-        participationVM.setUser(user);
-        participationVM.setCompetition(competition);
+        ParticipationVM participationVM = participationMapper.participationToParticipationVM(savedParticipation);
         return ResponseEntity.ok(participationVM);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ParticipationVM>> findAll(@RequestParam(defaultValue = "${pagination.defaultPage}") int page, @RequestParam(defaultValue = "${pagination.defaultPageSize}") int size) {
+        Page<Participation> participations = participationService.findAll(page, size);
+        Page<ParticipationVM> participationVMs = participations.map(p -> {
+            return participationMapper.participationToParticipationVM(p);
+        });
+        return ResponseEntity.ok(participationVMs);
+    }
+
+    @GetMapping("/result")
+    public List<ParticipationVM> getResults(@RequestParam(required = false) UUID competition_id, @RequestParam(required = false) UUID user_id) {
+        List<Participation> participations = participationService.findByCriteria(user_id, competition_id);
+        List<ParticipationVM> participationVMs = participations.stream().map(participationMapper::participationToParticipationVM).toList();
+        return participationVMs;
+    }
+
+    @GetMapping("/podium/{competition_id}")
+    public List<ParticipationVM> getCompetitionPodium(@PathVariable UUID competition_id) {
+        List<Participation> participations = participationService.competitionPodium(competition_id);
+        List<ParticipationVM> participationVMs = participations.stream().map(participationMapper::participationToParticipationVM).toList();
+        return participationVMs;
+    }
+
+    @GetMapping("/participation_history/{user_id}")
+    public List<ParticipationVM> getParticipationHistory(@PathVariable UUID user_id) {
+        List<Participation> participations = participationService.findParticipationHistory(user_id);
+        List<ParticipationVM> participationVMs = participations.stream().map(participationMapper::participationToParticipationVM).toList();
+        return participationVMs;
+    }
+
 }
